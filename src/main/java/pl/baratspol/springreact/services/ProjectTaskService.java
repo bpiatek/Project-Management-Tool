ed1c346f1154702586c1228b907dd6d4af1ebe5d
Bartosz Piatek <piatekbart@gmail.com>
2019-01-07 16:43:58 +0100

    add backlog controller with addProjectTaskToBAcklog method

---
+++ src/main/java/pl/baratspol/springreact/services/ProjectTaskService.java
@@ -0,0 +1,44 @@
+package pl.baratspol.springreact.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.baratspol.springreact.domain.Backlog;
+import pl.baratspol.springreact.domain.ProjectTask;
+import pl.baratspol.springreact.repositories.BacklogRepository;
+import pl.baratspol.springreact.repositories.ProjectTaskRepository;
+
+/**
+ * Created by Bartosz Piatek on 07/01/2019
+ */
+@Service
+public class ProjectTaskService {
+
+    private BacklogRepository backlogRepository;
+    private ProjectTaskRepository taskRepository;
+
+    @Autowired
+    public ProjectTaskService(BacklogRepository backlogRepository, ProjectTaskRepository taskRepository) {
+        this.backlogRepository = backlogRepository;
+        this.taskRepository = taskRepository;
+    }
+
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+        projectTask.setBacklog(backlog);
+        Integer backlogSequence = backlog.getProjectTaskSequence();
+        backlogSequence++;
+        backlog.setProjectTaskSequence(backlogSequence);
+        projectTask.setProjectSequence(projectIdentifier + "-" + backlogSequence);
+        projectTask.setProjectIdentifier(projectIdentifier);
+
+        if ( projectTask.getPriority() == null) {
+           projectTask.setPriority(3);
+        }
+
+        if (("".equals(projectTask.getStatus())) || projectTask.getStatus() == null) {
+            projectTask.setStatus("TO_DO");
+        }
+
+        return taskRepository.save(projectTask);
+    }
+}
---
+++ src/main/java/pl/baratspol/springreact/web/BacklogController.java
@@ -0,0 +1,44 @@
+package pl.baratspol.springreact.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import pl.baratspol.springreact.domain.ProjectTask;
+import pl.baratspol.springreact.services.MapValidationErrorService;
+import pl.baratspol.springreact.services.ProjectTaskService;
+
+import javax.validation.Valid;
+
+/**
+ * Created by Bartosz Piatek on 07/01/2019
+ */
+@RestController
+@RequestMapping("api/backlog")
+@CrossOrigin
+public class BacklogController {
+    private final ProjectTaskService taskService;
+    private final MapValidationErrorService validationErrorService;
+
+    @Autowired
+    public BacklogController(ProjectTaskService taskService, MapValidationErrorService validationErrorService) {
+        this.taskService = taskService;
+        this.validationErrorService = validationErrorService;
+    }
+
+    @PostMapping("{projectIdentifier}")
+    public ResponseEntity<?> addProjectTaskToBAcklog(
+            @Valid @RequestBody ProjectTask projectTask,
+            BindingResult result,
+            @PathVariable String projectIdentifier) {
+        ResponseEntity<?> errorMap = validationErrorService.mapValidationError(result);
+        if (errorMap != null) {
+            return errorMap;
+        }
+
+        ProjectTask projectTask1 = taskService.addProjectTask(projectIdentifier, projectTask);
+
+        return new ResponseEntity<>(projectTask1, HttpStatus.CREATED);
+    }
+}
